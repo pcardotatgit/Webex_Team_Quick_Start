3928d8a9d899a3614a3884d4bb3c427e5698f8ab
Patrick Cardot (pcardot) <pcardot@cisco.com>
2020-11-15 16:37:57 +0100

    PHP Webex team bot update

--- 3_php_bot_logic_server/bot_logic.php
+++ 3_php_bot_logic_server/bot_logic.php
@@ -7,16 +7,22 @@
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
-* open the [ debug.txt ] file in order to check dialog between this script and you bot
+* The goal is to make people familiar with Webex Team Bot and how they work
+*
+* open the [ debug.txt ] file in order to see the details of the dialog between this script and your Webex Team Bot
 */
 
 //error STOP uncomment here to stop the script in case of a loop
-
-// Set room_id : of the room thru which you communicate with your BOT
-$the_room_id="<Room_ID>";
-// Set the BOT Webex Token
-$accesstoken = '<Bot_Webex_Token>';
-
+$the_fichier="init_key_and_bot_id.txt";
+$fd = @fopen($the_fichier,"r");
+if (!$fd) die("the init_key_and_bot_id.txt file can't be openned");
+$ligne = fgets($fd, 300);	
+LIST($mot,$the_room_id)=explode(":",$ligne);
+echo 'Destination Room ID = '.$the_room_id;
+$ligne = fgets($fd, 300);	
+LIST($mot,$accesstoken)=explode(":",$ligne);
+echo '<br><br>Bot Webex Token = '.$accesstoken;	
+fclose($fd);
 
 $file='debug.txt';
 $fd = @fopen($file,"a+");
@@ -27,21 +33,40 @@ $jsonData = $jsonPost["data"]["id"];
  
 // Set variables. The accesstoken is taken from developer.ciscospark.com
 //$url = 'https://api.ciscospark.com/v1/messages/'.$jsonData; // old
-$url = "https://webexapis.com/v1/messages".$jsonData;	
-
- 
+$url = "https://webexapis.com/v1/messages/".$jsonData;	
+fputs($fd,"========================================================================================");
+fputs($fd,"\r\n");
+fputs($fd,date('j/m/y H:i:s'));
+fputs($fd,"\r\n");
+fputs($fd,"STEP 1 - Ok a message was received into the webex bot team room. Message ID is :");
+fputs($fd,"\r\n");
+fputs($fd,"\r\n");
+fputs($fd,"		".$jsonData); 
+fputs($fd,"\r\n"); 
+fputs($fd,"\r\n");
+fputs($fd,"STEP 2 - Build an HTTP header");
+fputs($fd,"\r\n");
 // Set HTTP POST headers
 $headr = array();
 $headr[] = 'Content-type: application/json';
 $headr[] = 'Authorization: Bearer '.$accesstoken;
-
-
-
-// 
-$replied_message="";
-
- 
-echo 'GET MESSAGE<br>'; 
+fputs($fd,"		OK DONE header is : ");
+fputs($fd,"\r\n");
+fputs($fd,"\r\n");
+fputs($fd,"		'Content-type': application/json");
+fputs($fd,"\r\n");
+fputs($fd,"		'Authorization': Bearer ".$accesstoken);
+fputs($fd,"\r\n"); 
+fputs($fd,"STEP 3 - Then get the sent message - Send a HTTP GET MESSAGE"); 
+fputs($fd,"\r\n"); 
+fputs($fd,"\r\n");
+fputs($fd,"		API Call is :"); 
+fputs($fd,"\r\n"); 
+fputs($fd,"\r\n");
+fputs($fd,"		".$url); 
+fputs($fd,"\r\n"); 
+fputs($fd,"\r\n");
+echo 'GET SENT MESSAGE<br>'; 
 // Send HTTP GET to obtain text message
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -50,64 +75,124 @@ curl_setopt($ch, CURLOPT_HTTPGET,true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 curl_close($ch);
- 
-// Extract the text message and who posted the message
-echo 'extract MESSAGE<br>';
-fputs($fd,'extract MESSAGE');
-fputs($fd,"\r\n");  
-fputs($fd,$response);
-fputs($fd,"\r\n");  
-$result = json_decode($response);
-$messageData = trim($result->{'text'});
-$messageUser = $result->{'personEmail'};
-echo 'MESSAGE : '.$messageData.'<br>';
-fputs($fd,'MESSAGE :');
-fputs($fd,"\r\n");  
-fputs($fd,$messageData);
+fputs($fd,"		OK Done JSON result is : ");
+fputs($fd,"=>\r\n"); 
 fputs($fd,"\r\n");
-// Define a string we will listen for and do something with
-if($messageData=="ping")
-{
-	fputs($fd,'In the ping loop');
-	fputs($fd,"\r\n"); 
-	$replied_message="Yeah !! I received your ping message !";
+fputs($fd,$response);
+fputs($fd,"<=\r\n"); 
+fputs($fd,"\r\n");
 
-	fputs($fd,'Message sent');
+
+if (stripos($response,'"errors":') != 0)
+{
+	fputs($fd,'ERROR = ');
+	fputs($fd,"\r\n");
+	fputs($fd,$response); 
 	fputs($fd,"\r\n"); 	
-}
-else if((stripos($messageData,"ping")!=false)&&(stripos($messageData,"bot:")!=false))
-{
-	$replied_message="PONG : [ Action for the BOT. An example of hyper link ](http://www.google.com)";
-
-}
-else if ((stripos($messageData,"hello")!=false)&&(stripos($messageData,"bot:")!=false))
-{
-	$replied_message="Hi Man, How are you ?";
-}
-else if ((stripos($messageData,"m fine")!=false)&&(stripos($messageData,"bot:")!=false))
-{
-	$replied_message="Perfect !";
-}
-else if ($messageData === "I don't understand this")
-{
-	$replied_message="";
-}
-else 
-{
-	$replied_message="I don't understand this";
-}
-
-if($replied_message!="")
-{
-	fputs($fd,'message to send = ');
-	fputs($fd,$replied_message); 
 	fputs($fd,"\r\n"); 	
-	//$url1 = "https://api.ciscospark.com/v1/messages";	  // old
+	echo 'ERROR MESSAGE RECEIVED FROM WEBEX<br>';
+	echo '<b>'.$response.'/<b>';
+	$replied_message='-bot: HELLO WORLD !';
+	echo '<h2>TRYING TO SEND A TEST MESSAGE :'.$replied_message.'</h2>';	
+	
 	$url1 = "https://webexapis.com/v1/messages";	 
 	$data = array("roomId" => $the_room_id, "markdown" => $replied_message);
 	$data_string = json_encode($data);
 	fputs($fd,'Reply orchestration to BOT: ');
 	fputs($fd,$replied_message);
+	fputs($fd,"\r\n");  
+	fputs($fd,"room id =".$the_room_id);
+	fputs($fd,"\r\n");  	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,$headr);
+	curl_setopt($ch, CURLOPT_POST,true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$response = curl_exec($ch);
+	curl_close($ch);	
+	echo '<h2>DID YOU RECEIVED IT IN THE WEBEX TEAM ROOM ?</h2>';
+	die();
+	
+}
+
+// Extract the text message and who posted the message
+echo 'extract MESSAGE<br>';
+fputs($fd,'		Let\'s extract the MESSAGE . The message is :');
+fputs($fd,"\r\n");
+$result = json_decode($response);
+$messageData = trim($result->{'text'});
+$messageUser = $result->{'personEmail'};
+echo 'MESSAGE : '.$messageData.'<br>';
+fputs($fd,"\r\n"); 
+fputs($fd,"\r\n");  
+fputs($fd,"		=> \r\n");
+fputs($fd,"			".$messageData);
+fputs($fd,"\r\n		<=");
+fputs($fd,"\r\n");
+fputs($fd,'Step 4 - Let\'s parse the MESSAGE . And send back a message ');
+fputs($fd,"\r\n");
+fputs($fd,"\r\n");
+
+if(stripos($messageData,"bot:")==false)
+{
+	// Define a string we will listen for and do something with
+	if(($messageData=="ping")&&(stripos($messageData,"bot:")==false))
+	{
+		fputs($fd,'		message = ping : We are In the "ping" if branch');
+		fputs($fd,"\r\n"); 
+		$replied_message="-bot: => Yeah !! I received your ping message !";
+		fputs($fd,'		Replied Message will be : "bot: => Yeah !! I received your ping message !"');
+		fputs($fd,"\r\n"); 	
+	}
+	else if((stripos($messageData,"ping")!=false)&&(stripos($messageData,"bot:")!=false))
+	{
+		$replied_message="-bot: PONG : [ Action for the BOT. An example of hyper link ](http://www.google.com)";
+	}
+	else if ((stripos($messageData,"hello")!=false)&&(stripos($messageData,"bot:")==false))
+	{
+		$replied_message="-bot: Hi Man, How are you ?";
+	}
+	else if ((stripos($messageData,"m fine")!=false)&&(stripos($messageData,"bot:")!=false))
+	{
+		$replied_message="Perfect !";
+	}
+	else if ($messageData === "-bot: => I don't understand this")
+	{
+		$replied_message="";
+	}
+	else if ($messageData === "")
+	{
+		$replied_message="-bot: PONG !";
+	}
+	else if ($messageData === "PONG !")
+	{
+		$replied_message="";
+	}
+	else 
+	{
+		$replied_message="-bot: I don't understand this";
+	}
+}
+else
+{	
+	fputs($fd,"\r\n"); 	
+	fputs($fd,'		This message is an answer from the BOT.  Don\'t process it ');
+	fputs($fd,"\r\n"); 	
+	fputs($fd,"\r\n"); 	
+}
+if($replied_message!="")
+{
+	fputs($fd,"\r\n"); 	
+	fputs($fd,"\r\n"); 	
+	fputs($fd,'		message to send = ');
+	fputs($fd,"\r\n"); 	
+	fputs($fd,"\r\n"); 	
+	//$url1 = "https://api.ciscospark.com/v1/messages";	  // old
+	$url1 = "https://webexapis.com/v1/messages";	 
+	$data = array("roomId" => $the_room_id, "markdown" => $replied_message);
+	$data_string = json_encode($data);
+	fputs($fd,"		".$replied_message);
 	fputs($fd,"\r\n");  
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url1);
@@ -117,10 +202,16 @@ if($replied_message!="")
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$response = curl_exec($ch);
 	curl_close($ch);
+	fputs($fd,"\r\n"); 	
+	fputs($fd,"\r\n"); 	
+	fputs($fd,'		OK Message Sent to webex team room ');
+	fputs($fd,"\r\n"); 	
+	fputs($fd,'		'.$response);
+	fputs($fd,"\r\n"); 		
 	fclose($fd);
 	$replied_message="";
 }
 
-echo 'OK';
+echo 'OK end of script';
 
 ?>
--- 3_php_bot_logic_server/readme.md
+++ 3_php_bot_logic_server/readme.md
@@ -1,10 +1,51 @@
-#what is it#
+# what is this code 
 
-This script is a basic logic for start / test your first Webex Team bot.
+The **bot_logic.php** PHP script is a basic logic for very light Webex Team bot.
 
-This script is a PHP script. You must run it into any PHP web server.  Just copy and paste the script into a sub folder of your PHP web server and that's it.
+This script generate a **debug.txt** file which helps you to troubleshoot your bot if something doesn't work.
 
-After that configure a WebHook for your Webex Team Bot, And use the public URL of your bot logic as the webhook target URL ( 
+This script is a PHP script. That means that you must run it into any PHP web server.  
+
+The first step is to install a PHP web server. I recommand XAMPP because it is very easy to install, on any operating system. It is very easy to use.
+
+But for those of you who are familiar with such things, we just need either lighttpd, or appache, or nginx  and PHP installed with it and that's it.
+Add a SQLite or MySQL database behind it will allow you to developp robust and powerfull server applications.
+
+
+## Installation
+
+### The PHP Script
+
+Just copy and paste the PHP **bot_logic.php** script into a sub folder of your PHP web server and that's it.
+
+For example if you installed Apache :
+
+Copy the script for example into :
+
+    htdocs\my_webex_team_bot\bot_logic.php
+
+### Make your web server publickly availble
+
+This is mandatory to make the Webex Team Bot Webhook to work. Webex Team must be able to reach your web server.
+
+**For now let's install NGROK**
+
+We assume here that you installed your Web Server into your laptop.  Which is the best to develop and test your bot logic before hosting it  anywhere else.
+
+follow the bellow instruction for installing NGROK, and make you bot logic available to Webex Team in less than 5 minutes
+
+[Install NGROK](https://developer.cisco.com/learning/lab/collab-spark-botkit/step/4)
+
+
+- **1 - Start Your Web Server**
+- **2 - Start NGROK**
+- **3 - Open You Browser onto the NGROK Generated Public Domain Name**
+
+You should have access to your PHP webserver.
+
+**Configure your Bot logic URL in Your Webex Team Bot**
+
+After that go to Webex Team and configure a WebHook for your Webex Team Bot. Use the public URL of your bot logic as the webhook target URL ( 
 
 In developer.webex.com go to **Webhooks** :
 
@@ -14,10 +55,11 @@ In developer.webex.com go to **Webhooks** :
 - resources = messages
 - event = created
 
+**Adjust the init_key_and_bot_id.txt file configuration**
 In this PHP script You must configure :
 
-- <Room_ID> of the Webex Team room into which youn communicate with your bot
-- <Bot_Webex_Token> : The Webex Team bot's token
+- <DESTINATION_ROOM_ID> of the Webex Team room into which youn communicate with your bot
+- <BOT_ACCESS_TOKEN> : The Webex Team bot's token
 
 And you are ready to go !
 
@@ -27,19 +69,23 @@ Open Webex Team, go to the BOT Room and send **ping**
 
 Your bot should reply to you :
 
-**Yeah !! I received your ping message !**
+**-bot: Yeah !! I received your ping message !**
 
 For debugging, open the **debug.txt** in the same directory as your php script
 
-After that you can send :
+After that you can send some commands to your script like:
 
-**@bot: ping**
+**ping**
 
-**@bot: hello**
+**-hello**
 
-**@bot: ( with something )** intent to send to the bot a message to compute.
+**-with something** intent to send to the bot a message to compute.
 
-But look into the code and make any changes you want to make your bot smart
+Look into the code at line 137 (**if(stripos($messageData,"bot:")==false)**
+
+The message parser starts here and it is almost empty.   You have make your changes if you want to make your bot to be smart
+
+Have a look how this if statement is written
 
 #tips#
 
